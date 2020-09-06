e4f627ec411ac47d51c7a981ade3f371e200dd61
MakielZX <marcoselcrack45@gmail.com>
2020-09-06 12:25:55 -0400

    Empezando la biblioteca

--- app/vistas/inc/header.php
+++ app/vistas/inc/header.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Peta&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
     <link href='https://css.gg/user.css' rel='stylesheet'>
 
     <title>Document</title>
@@ -31,7 +32,13 @@
     </form>
     <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
         <li class="nav-item active ml-3 ">
-          <a class="nav-link text-light bold" href="<?php RUTA_URL?>bibliotecaController">Biblioteca <span class="sr-only">(current)</span></a>
+          <a class="nav-link text-light bold" href="<?php RUTA_URL?>bibliotecaController">
+            Ofertas
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart-dash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                <path fill-rule="evenodd" d="M6 7.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+          </a>
         </li>
         <li class="nav-item ml-3">
           <a class="nav-link text-light bold" href="#">Ofertas</a>
--- app/vistas/paginas/inicio.php
+++ app/vistas/paginas/inicio.php
@@ -1,9 +1,10 @@
 <?php require_once '../app/vistas/inc/header.php'; ?>
 
 <div style="height: 30rem;" class="container-fluid upfront mb-2">
+     
     <div class="container-xl row">
-        <div style="height: 26rem;" class="col-7 mt-3 container">
-            <div style="border: 0.1rem solid white;" id="carouselFront" class="carousel slide mt-2" data-ride="carousel">
+        <div style="height: 26rem;" class="col-7 mt-5 container-sm">
+            <div id="carouselFront" class="carousel slide mt-2 border" data-ride="carousel">
                     <div class="carousel-inner">
                             <div class="carousel-item active">
                                 <img src="img/fifa21.jpg" alt="">
@@ -26,37 +27,48 @@
             </div>
         </div>
         
-        <div style="border-radius: 0.5%; align-items: center; height: 12rem;" class="col-5 mt-4 ml-0 container-xl bg-light">
-           <div class="row">
-                <div style="font-family: 'Lato';" class="col-12 container mb-2 mt-1 bold align-content-center">
-                    <h6 class="h6">Categorias</h6>
-                </div>
-                <div class="col-3 mb-2">
-                    <h6>Equipo</h6>
+        <div id="generalPanel" class="col-xl-4 container mt-5 categoriesElements generalPanel border row">
+           <div class="col-sm-6 border-right">
+               <div class="categoriesElements mt-2">
+                    <h6>Gaming</h6>
                     <a href="">
                         <img src="img/keyboardMK1.jpg" alt="">
                     </a>
+                    <div class="container">
+                        <ul class="categoriesList"> 
+                            <li><a href="">Computadoras</a></li>
+                            <li><a href="">Headsets</a></li>
+                            <li><a href="">Teclados</a></li>
+                        </ul>
+                    </div> 
+                 </div>
+           </div>
+           <div class="col-sm-6">
+               <div class="categoriesElements mt-2">
+                    <h6>Electronicos</h6>
+                    <a href="">
+                    <img src="img/mouseAntryx.jpg" alt="">
+                    </a>
+                    <div class="container">
+                        <ul class="categoriesList"> 
+                            <li><a href="">Computadoras</a></li>
+                            <li><a href="">Smartphones</a></li>
+                            <li><a href="">Camaras</a></li>
+                        </ul>
+                    </div>
                 </div>  
-                <div class="col-3 mb-2">
-                    <a href="">
-                        <img src="img/keyboardMK1.jpg" alt="">
-                    </a>
-                </div>  
-                <div class="col-3 mb-2">
-                    <a href="">
-                        <img src="img/keyboardMK1.jpg" alt="">
-                    </a>
-                </div>   
            </div>
         </div>
-
-     
+        <div class="col-1">
+        </div>
     </div>
 </div>
 
-<div class="container-sm">
-    <h1 style="font-family: 'Nunito', sans-serif;" class="display-2">Rebajas</h1>
+<div class="container">
+    <span id="welcomeText" class="h2">Lo mismo de siempre, pero más barato</span>
 </div>
+
+<br>
 
 <div class="container-md mt-4">
     <div class="row">
@@ -75,8 +87,11 @@
                 <img class="blackBorder" style="width: 20rem; height: 11.75rem;"src="img/op4.jpg" alt="">
             </a>
         </div>
-        
     </div>
+</div>
+
+<div class="container-sm">
+    <h1 style="font-family: 'Nunito', sans-serif;" class="display-3">Rebajas</h1>
 </div>
 <br>
 
